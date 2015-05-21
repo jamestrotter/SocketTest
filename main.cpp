344c138a1cce608d2a300e37ac27d0e870ec5783
trotskiftw <trotskiftw@googlemail.com>
2015-05-21 11:08:44 -0400

    Changes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,51 @@
-#include <websocketpp/config/asio_no_tls_client.hpp>
-#include <websocketpp/client.hpp>
-
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
 #include <websocketpp/common/thread.hpp>
-#include <websocketpp/common/memory.hpp>
 
 #include <stdio.h>
-#include <cstdlib>
-#include <iostream>
-#include <map>
-#include <string>
-#include <sstream>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+using websocketpp::connection_hdl;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+using websocketpp::lib::thread;
+using websocketpp::lib::mutex;
+using websocketpp::lib::unique_lock;
+using websocketpp::lib::condition_variable;
+
+
+enum action_type {
+    SUBSCRIBE,
+    UNSUBSCRIBE,
+    MESSAGE
+};
+
+struct action {
+    action(action_type t, connection_hdl h) : type(t), hdl(h) {}
+    action(action_type t, connection_hdl h, server::message_ptr m)
+      : type(t), hdl(h), msg(m) {}
+
+    action_type type;
+    websocketpp::connection_hdl hdl;
+    server::message_ptr msg;
+};
 
 int main()
 {
-    printf("Hello World\n");
+    printf("Starting Server...\n");
+	uint16_t port = 8888;
+	server m_server;
+	
+	m_server.init_asio();
+	m_server.listen(port);
+	m_server.start_accept();
+	
+	try {
+		m_server.run();
+	} catch (const std::exception & e) {
+		std::cout << e.what() << std::endl;
+	}
     return 0;
 }
---
+++ run.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+LD_LIBRARY_PATH="boost_1_58_0/stage/lib" ./bin/exe

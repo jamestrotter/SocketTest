108a98b895fa89254badfab7a840850fc0540e7d
trotskiftw <trotskiftw@googlemail.com>
2015-05-21 09:59:38 -0400

    Added boost

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
+
+#include <websocketpp/common/thread.hpp>
+#include <websocketpp/common/memory.hpp>
+
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
